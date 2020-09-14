9cb4c8c080629b3ff1168c80324ab0419e1c5bc5
L <L862608263@163.com>
2020-09-14 10:00:53 +0800

    update firebase upload sh

--- UploadDSYMToFrirebase.sh
+++ UploadDSYMToFrirebase.sh
@@ -1,6 +1,6 @@
-# 保证 appDsyms.zip, GoogleService-Info.plist, GoogleService-Info.plist, upload.sh 在同一个目录下
+# 保证 appDsyms.zip, GoogleService-Info.plist, 此文件 在同一个目录下
 # 替换对应版本的 appDsyms文件
 # 最好保持upload-symbols的版本更新
 # /Pods/Crashlytics/iOS/Crashlytics.framework/upload-symbols 
-# 控制台直接执行upload.sh 上传
+# 控制台直接执行此文件 上传
 ./upload-symbols -gsp ./GoogleService-Info.plist -p ios ./appDsyms.zip
